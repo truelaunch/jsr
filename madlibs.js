52b1532107eca8516707496424303a5d49b54ef8
Joanna Bennett <joanna@mandrill.com>
2018-05-15 16:02:35 -0400

    create basic madlibs functionality

--- madlibs.js
+++ madlibs.js
@@ -1,2 +1,9 @@
 // Madlibs homework instructions
 // https://gist.github.com/sonylnagale/47bb2aa56b37baa7515be344d10264ac
+
+var companies = ["Uber", "Lyft", "Google", "Apple", "MailChimp"];
+var animals = ["monkeys", "dogs", "giraffes", "emus", "sloths"];
+
+for (var i=0; i < companies.length; i++) {
+  console.log(`My company is an ${companies[i]} for ${animals[i]}!`);
+}
