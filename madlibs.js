736b10dfcc55912deb8933ba8ca60deeaff4837d
Joanna Bennett <joanna@mandrill.com>
2018-05-15 16:48:37 -0400

    randomize companies and animals that are output

--- madlibs.js
+++ madlibs.js
@@ -4,6 +4,17 @@
 var companies = ["Uber", "Lyft", "Google", "Apple", "MailChimp"];
 var animals = ["monkeys", "dogs", "giraffes", "emus", "sloths"];
 
+// generate a random number from 1 - maxNum (which will always be 1 less than the param entered due to math.floor)
+// use math.floor to round, since we want 0 - 4 as the range for the 0 indexed array.
+function getRandomNum(maxNum) {
+  return Math.floor(Math.random() * maxNum);
+}
+
+
 for (var i=0; i < companies.length; i++) {
-  console.log(`My company is an ${companies[i]} for ${animals[i]}!`);
+  // Generate 2 random numbers
+  // 5 is the parameter, since there are 5 items in each array
+  var randomA = getRandomNum(5);
+  var randomB = getRandomNum(5);
+  console.log(`My company is an ${companies[randomA]} for ${animals[randomB]}!`);
 }
