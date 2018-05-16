303dcc57f9f6f4c9a7582b28ee7ad4d1464481fd
Joanna Bennett <joanna@mandrill.com>
2018-05-15 20:19:48 -0400

    if/else stmt related to age

---
+++ class3.js
@@ -0,0 +1,17 @@
+var age = 14;
+
+if (age >= 62) {
+  console.log("you've got social security benefits");
+} else if (age >= 35) {
+  console.log("you can run for president");
+} else if (age >= 25) {
+  console.log("you can rent a car");
+} else if (age >= 21) {
+  console.log("you can drink alcohol");
+} else if (age >= 18) {
+  console.log("you can vote");
+} else if (age >= 16) {
+  console.log("you can drive");
+} else {
+  console.log("you only go to school");
+}
