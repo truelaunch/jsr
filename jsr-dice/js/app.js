e7b8ee0bf309924c8f8b1697d1ebea87cfac8f77
Joanna Bennett <joanna@mandrill.com>
2018-05-15 22:57:14 -0400

    clone jsr-dice exercise files

---
+++ jsr-dice/js/app.js
@@ -0,0 +1,17 @@
+/*
+
+Create a page where every time the user hits the "Roll Dice" button, the screen randomly updates the two dice. 
+Use the html and css code included in the starter code folder to get started.
+
+Follow the steps below to write your code.
+    * generate a random number between 1 - 6 and store to a variable, random1
+    * generate a random number between 1 - 6 and store to a variable, random2
+    * combine 'dice-' and random1 to form the random class for the first die, firstDie
+    * combine 'dice-' and random2 to form the random class for the second die, secondDie
+    * get the first die by ID and update the class to firstDie (hint: document.getElementById)
+    * get the first die by ID and update the class to secondDie (hint:document.getElementById)
+
+Check to see if the "Roll the Dice" button has been clicked; if it has, run the diceRoll function 
+(hint: document.getElementById)
+
+*/
