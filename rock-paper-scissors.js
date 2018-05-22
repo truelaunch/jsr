8465e1652df12d3fd68ffa2693cd4e10647b4b73
Joanna Bennett <joanna@mandrill.com>
2018-05-21 23:34:32 -0400

    run game 3 times

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,86 +10,67 @@
 // Output the winner
 // Keep in mind the major concepts we've learned so far: control structures, functions, scope, and objects. Utilize whatever structures you feel appropriate.
 
-// OBJECTS
-var rock = {
-    stringRef: "rock",
-    losesTo: "paper",
-    winsTo: "scissors"
-}
-
-var paper = {
-    stringRef: "paper",
-    losesTo: "scissors",
-    winsTo: "rock"
-}
-
-var scissors = {
-    stringRef: "scissors",
-    losesTo: "rock",
-    winsTo: "paper"
-}
-
-// Global Variables:
-var computerOptions = ["rock", "paper", "scissors"];
-var computerChoice = computerOptions[getRandomNum()];
-var userChoice = prompt("Enter either rock, paper, or scissors");
-
-// function to generate a random number from 0 - 2 to utilize with options array
-function getRandomNum(maxNum) {
-  return Math.floor(Math.random() * 3);
-}
-
-// userInput is a string, since that is what a prompt returns.
-// This function will convert the string into the var name that properly references the object
-function convertUserChoice(userInput) {
-  if (userInput === "rock") {
-    return rock;
-  } else if (userInput === "scissors") {
-    return scissors;
-  } else if (userInput === "paper") {
-    return paper;
-  } else {
-    console.log("Please enter a valid option. Refresh to try again.");
+var runGame = function() {
+  // OBJECTS
+  var rock = {
+      stringRef: "rock",
+      losesTo: "paper",
+      winsTo: "scissors"
   }
-}
 
-function determineWinner(user, computer) {
-  if (user.stringRef === computer) {
-    console.log("It's a tie!");
-  } else if(user.losesTo === computer) {
-    console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-  } else if(user.winsTo === computer) {
-    console.log(`You win! ${userChoice} beats ${computerChoice}`);
-  } else {
-    // Error state
-    console.log("Please enter a valid option. Refresh to try again.");
+  var paper = {
+      stringRef: "paper",
+      losesTo: "scissors",
+      winsTo: "rock"
   }
+
+  var scissors = {
+      stringRef: "scissors",
+      losesTo: "rock",
+      winsTo: "paper"
+  }
+
+  // Global Variables:
+  var computerOptions = ["rock", "paper", "scissors"];
+  var computerChoice = computerOptions[getRandomNum()];
+  var userChoice = prompt("Enter either rock, paper, or scissors");
+
+  // function to generate a random number from 0 - 2 to utilize with options array
+  function getRandomNum(maxNum) {
+    return Math.floor(Math.random() * 3);
+  }
+
+  // userInput is a string, since that is what a prompt returns.
+  // This function will convert the string into the var name that properly references the object
+  function convertUserChoice(userInput) {
+    if (userInput === "rock") {
+      return rock;
+    } else if (userInput === "scissors") {
+      return scissors;
+    } else if (userInput === "paper") {
+      return paper;
+    } else {
+      console.log("Please enter a valid option. Refresh to try again.");
+    }
+  }
+
+  function determineWinner(user, computer) {
+    if (user.stringRef === computer) {
+      console.log("It's a tie!");
+    } else if(user.losesTo === computer) {
+      console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
+    } else if(user.winsTo === computer) {
+      console.log(`You win! ${userChoice} beats ${computerChoice}`);
+    } else {
+      // Error state
+      console.log("Please enter a valid option. Refresh to try again.");
+    }
+  }
+
+  determineWinner(convertUserChoice(userChoice), computerChoice);
 }
 
-determineWinner(convertUserChoice(userChoice), computerChoice);
-
-// if (userChoice === computerChoice) {
-//   console.log("It's a tie!");
-// } else if (userChoice === "rock") {
-//     if (rock.losesTo === computerChoice) {
-//       console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-//     } else {
-//       console.log(`You win! ${userChoice} beats ${computerChoice}`);
-//     }
-// } else if (userChoice === "paper") {
-//     if (paper.losesTo === computerChoice) {
-//       console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-//     } else {
-//       console.log(`You win! ${userChoice} beats ${computerChoice}`);
-//     }
-// } else if (userChoice === "scissors") {
-//     if (scissors.losesTo === computerChoice) {
-//       console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-//     } else {
-//       console.log(`You win! ${userChoice} beats ${computerChoice}`);
-//     }
-// }
-//   // error state. Invalid entry
-//   else if (!options.includes(userChoice)) {
-//     console.log("Please enter a valid option. Refresh to try again.");
-// }
+// Run game 3 times
+for (var i=0; i<3; i++) {
+  runGame();
+}
