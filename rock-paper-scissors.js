a2e30ab0c7e0affdc93593e6e98667d601033f7e
Joanna Bennett <joanna@mandrill.com>
2018-05-21 23:30:16 -0400

    create function to determine winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,23 +12,26 @@
 
 // OBJECTS
 var rock = {
+    stringRef: "rock",
     losesTo: "paper",
     winsTo: "scissors"
 }
 
 var paper = {
+    stringRef: "paper",
     losesTo: "scissors",
     winsTo: "rock"
 }
 
 var scissors = {
+    stringRef: "scissors",
     losesTo: "rock",
     winsTo: "paper"
 }
 
 // Global Variables:
-var options = ["rock", "paper", "scissors"];
-var computerChoice = options[getRandomNum()];
+var computerOptions = ["rock", "paper", "scissors"];
+var computerChoice = computerOptions[getRandomNum()];
 var userChoice = prompt("Enter either rock, paper, or scissors");
 
 // function to generate a random number from 0 - 2 to utilize with options array
@@ -36,28 +39,57 @@ function getRandomNum(maxNum) {
   return Math.floor(Math.random() * 3);
 }
 
-if (userChoice === computerChoice) {
-  console.log("It's a tie!");
-} else if (userChoice === "rock") {
-    if (rock.losesTo === computerChoice) {
-      console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-    } else {
-      console.log(`You win! ${userChoice} beats ${computerChoice}`);
-    }
-} else if (userChoice === "paper") {
-    if (paper.losesTo === computerChoice) {
-      console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-    } else {
-      console.log(`You win! ${userChoice} beats ${computerChoice}`);
-    }
-} else if (userChoice === "scissors") {
-    if (scissors.losesTo === computerChoice) {
-      console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
-    } else {
-      console.log(`You win! ${userChoice} beats ${computerChoice}`);
-    }
-}
-  // error state. Invalid entry
-  else if (!options.includes(userChoice)) {
+// userInput is a string, since that is what a prompt returns.
+// This function will convert the string into the var name that properly references the object
+function convertUserChoice(userInput) {
+  if (userInput === "rock") {
+    return rock;
+  } else if (userInput === "scissors") {
+    return scissors;
+  } else if (userInput === "paper") {
+    return paper;
+  } else {
     console.log("Please enter a valid option. Refresh to try again.");
+  }
 }
+
+function determineWinner(user, computer) {
+  if (user.stringRef === computer) {
+    console.log("It's a tie!");
+  } else if(user.losesTo === computer) {
+    console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
+  } else if(user.winsTo === computer) {
+    console.log(`You win! ${userChoice} beats ${computerChoice}`);
+  } else {
+    // Error state
+    console.log("Please enter a valid option. Refresh to try again.");
+  }
+}
+
+determineWinner(convertUserChoice(userChoice), computerChoice);
+
+// if (userChoice === computerChoice) {
+//   console.log("It's a tie!");
+// } else if (userChoice === "rock") {
+//     if (rock.losesTo === computerChoice) {
+//       console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
+//     } else {
+//       console.log(`You win! ${userChoice} beats ${computerChoice}`);
+//     }
+// } else if (userChoice === "paper") {
+//     if (paper.losesTo === computerChoice) {
+//       console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
+//     } else {
+//       console.log(`You win! ${userChoice} beats ${computerChoice}`);
+//     }
+// } else if (userChoice === "scissors") {
+//     if (scissors.losesTo === computerChoice) {
+//       console.log(`Sorry, the computer beat you! ${computerChoice} beats ${userChoice}`);
+//     } else {
+//       console.log(`You win! ${userChoice} beats ${computerChoice}`);
+//     }
+// }
+//   // error state. Invalid entry
+//   else if (!options.includes(userChoice)) {
+//     console.log("Please enter a valid option. Refresh to try again.");
+// }
