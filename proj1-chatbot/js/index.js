4b76a941fbb51cd61559af803b0a63953deb1e56
Joanna Bennett <joanna@mandrill.com>
2018-05-29 11:06:55 -0400

    begin convo once DOM loaded

--- proj1-chatbot/js/index.js
+++ proj1-chatbot/js/index.js
@@ -9,11 +9,17 @@
 
 
 // Global vars:
+var rosieChatPane = document.getElementById("rosie");
 var wordsHi = ["hello", "hey", "hi"];
+var wordsWeather = ["weather", "outside", "temperature", "conditions", "forecast"];
 var wordsDinner = ["supper", "dinner", "eating", "eat"];
 var wordsThanks = ["thanks", "thank"];
-var wordsBye = ["bye", "goodbye"];
+var wordsBye = ["bye", "goodbye", "later"];
 
+// Once DOM loaded:
+document.addEventListener("DOMContentLoaded", function(event) {
+  beginConvo();
+});
 
 document.getElementById('chatForm').addEventListener('submit', function(e) {
   e.preventDefault();
@@ -23,6 +29,10 @@ document.getElementById('chatForm').addEventListener('submit', function(e) {
   textInput.value = "";
   parseJaneWords(janeMsg);
 });
+
+function beginConvo() {
+  rosieChatPane.innerHTML += `<li>Hi Judy!</li>`;
+}
 
 function parseJaneWords(janeWords) {
   // put all words entered into an array, replacing non alpha-numeric chars with a blank string
@@ -40,23 +50,24 @@ function rosieResponse(wordsEntered) {
   // If so, rosie will respond appropriately
   for (var i=0; i < wordsEntered.length; i++) {
     if (wordsHi.includes(wordsEntered[i])) {
-      document.getElementById("rosie").innerHTML += `<li>Hi Judy!</li>`;
+      rosieChatPane.innerHTML += `<li>What can I help you with today, Judy?</li>`;
+      return;
+    }
+    else if (wordsWeather.includes(wordsEntered[i])) {
+      rosieChatPane.innerHTML += `<li>Today's forecase is cloudy with a chance of asteroids.</li>`;
       return;
     }
     else if (wordsDinner.includes(wordsEntered[i])) {
-      document.getElementById("rosie").innerHTML += `<li>I'll be serving spaceballs and spaghetti.</li>`;
+      rosieChatPane.innerHTML += `<li>I'll be serving spaceballs and spaghetti.</li>`;
       return;
     }
     else if (wordsThanks.includes(wordsEntered[i])) {
-      document.getElementById("rosie").innerHTML += `<li>You're very welcome, Judy!</li>`;
+      rosieChatPane.innerHTML += `<li>You're very welcome, Judy!</li>`;
       return;
     }
     else if (wordsBye.includes(wordsEntered[i])) {
-      document.getElementById("rosie").innerHTML += `<li>Goodbye Judy! It was nice chatting.</li>`;
+      rosieChatPane.innerHTML += `<li>Goodbye Judy! It was nice chatting.</li>`;
       return;
     }
-    else {
-      document.getElementById("rosie").innerHTML += `<li>I'm sorry, Judy. I don't understand what you're saying.</li>`;
-    }
-  }
+  }  
 }
