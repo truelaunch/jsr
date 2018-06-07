eeedb3d9957178cfeead8c2ca41142f3d0f74794
Joanna Bennett <joanna@mandrill.com>
2018-06-07 16:59:48 -0400

    create 2 instances of bird

--- class9/farm/app.js
+++ class9/farm/app.js
@@ -1,27 +1,50 @@
 // prototype
-var Bird = function() {
-  this.mouth = "beak";
-  this.name = "bird";
-  this.image = "bird image";
-  this.talk = "chirp chirp";
+var FarmAnimal = function() {
+  this.name;
+  this.image;
+  this.talk;
 }
 
 // sub classes
-var Turkey = function() {
+var Bird = function() {
+  FarmAnimal.call(this);
+  this.mouth = "beak";
+  this.img = "bird image";
+  this.talk = "chirp chirp";
+}
+
+var Turkey = function(name) {
   Bird.call(this);
-  this.name = "turkey";
-  this.image = "turkey image";
+  this.name = name;
+  this.image = "http://www.freepngimg.com/download/turkey_bird/5-2-turkey-png.png";
   this.talk = "gobble gobble";
 }
 
-var Chicken = function() {
+var Chicken = function(name) {
   Bird.call(this);
-  this.name = "chicken";
-  this.image = "chicken image";
+  this.name = name;
+  this.image = "https://i2.wp.com/myfoodyshop.com/wp-content/uploads/2017/03/cobb700.png?fit=400%2C529&ssl=1";
   this.talk = "cluck cluck";
 }
 
 // instances
-var henry = new Chicken();
+var henry = new Chicken("Henry");
+var thomas = new Turkey("Thomas");
 
-console.log(henry);
+// animal node vars
+var chickenNode = document.getElementById("chicken");
+var turkeyNode = document.getElementById("turkey");
+
+//images for each animal
+chickenNode.style.backgroundImage = `url(${henry.image})`;
+turkeyNode.style.backgroundImage = `url(${thomas.image})`;
+
+
+// event listeners
+chickenNode.addEventListener("click", function() {
+  alert(`${henry.talk}`);
+})
+
+turkeyNode.addEventListener("click", function() {
+  alert(`${thomas.talk}`);
+})
