787ec904d995fb426fec6c868a3d2c45c9fa6f35
Joanna Bennett <joanna@mandrill.com>
2018-06-07 20:16:45 -0400

    class 10 exercise starter file

---
+++ class10/stickies/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // first select your container and set it to a variable so you don't need to find it every time
+
+  // we need a counter in this scope to count our stickies
+
+  // next, a click handler for creaing the sticky
+    // we need to create a new box each click
+    // create a the sticky markup
+    // get the user input (hint: the .val method)
+    // set the color of the sticky from user input (hint: the .css() method)
+    // use your closure to increment the number of stickies
+    // stick your sticky in the DOM
+});
