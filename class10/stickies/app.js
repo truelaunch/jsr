e059a0dab2037f4b3830623159babcdd07a1ae86
Joanna Bennett <joanna@mandrill.com>
2018-06-07 21:11:20 -0400

    completed sticky exercise

--- class10/stickies/app.js
+++ class10/stickies/app.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   // first select your container and set it to a variable so you don't need to find it every time
+  var container = $(".container");
 
   // we need a counter in this scope to count our stickies
-
+  var counter = 0;
   // next, a click handler for creaing the sticky
-    // we need to create a new box each click
-    // create a the sticky markup
+  var btn = $(".box-creator-button");
+
+  // we need to create a new box each click
+  btn.click(function() {
     // get the user input (hint: the .val method)
-    // set the color of the sticky from user input (hint: the .css() method)
+    var colorInput = $(".box-color-input").val();
+    // get message from input
+    var msgInput = $(".box-color-note").val();
+    createBox(colorInput, msgInput);
+  });
+
+  function createBox(color, msg) {
     // use your closure to increment the number of stickies
+    counter+=1;
+    // set the color of the sticky from user input (hint: the .css() method)
+    // add message to sticky
     // stick your sticky in the DOM
+    var sticky = container.append(`<div class="box" style="background: ${color}">${counter}. ${msg}</div>`);
+  }
 });
