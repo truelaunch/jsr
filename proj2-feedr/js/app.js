131e42f1927cbc506b1bb487fa9d168816e098d3
Joanna Bennett <joanna@mandrill.com>
2018-06-19 19:52:50 -0400

    some psuedo code for proj 2 logic

--- proj2-feedr/js/app.js
+++ proj2-feedr/js/app.js
@@ -1,5 +1,12 @@
 /* Feedr */
+// GENERAL QUESTIONS:
+// What should I do re article images?
+// What about "impressions"? Can that be date instead?
+// How do I deal with multiple API calls for 3 different news sources??
+// Continue with click function for h3 below
 
+// THE GUARDIAN API INFO
+// put this in an object named API sources. The Guardian is the key with sub-object with this info:
 var root = "https://content.guardianapis.com/search";
 var key = "830e55a9-fe2e-4cb2-9de4-0ba66be99e23";
 var section = "technology";
@@ -11,6 +18,11 @@ $.ajax({
 	console.log(data);
   addContentToDom(data);
 });
+
+// generic function that takes into account the name of what's inside the selection.
+// use data-attribute in html
+// trigger the code based on that to read what's inside the APIs response
+// front page triggers the first one.
 
 function addContentToDom(data) {
   // response object. results is an object with 10 articles
@@ -30,6 +42,10 @@ function addContentToDom(data) {
 $(".articleContent h3").click(function() {
   console.log(this);
   $("#popUp").removeClass("hidden");
+
+  // How to know which article was clicked so that article's content gets populated into popup?
+  // Use data-attribute to connect the HTML and the JS
+  // .parent().data("whatever")  <--
 });
 
 $(".closePopUp").click(function() {
